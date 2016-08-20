d1751ad04bf3c1553abad3c7d6beedff368bd6af
Renan Gurgel <gurgel.renan@gmail.com>
2016-08-20 00:11:00 -0300

    Add sendgrid

--- app/mailers/scheduler_mailer.rb
+++ app/mailers/scheduler_mailer.rb
@@ -1,2 +1,9 @@
 class SchedulerMailer < ApplicationMailer
+  default :from => 'renan.gurgel@agendakidsdigital.com'
+
+  def send_signup_email(lead)
+    @lead = lead
+    mail( :to => @lead.email,
+    :subject => 'Thanks for signing up for our amazing app' )
+  end
 end
