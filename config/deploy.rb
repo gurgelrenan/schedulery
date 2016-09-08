557358cc04b177d787e2985ed959b293a47b6411
Renan Gurgel <gurgel.renan@gmail.com>
2016-09-07 21:47:08 -0300

    remo bin from linked dir

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,9 +5,9 @@ set :application, 'schedulery'
 set :repo_url, 'git@github.com:gurgelrenan/schedulery.git'
 set :branch, :master
 set :deploy_to, '/home/deploy/schedulery'
-set :pty, true
+# set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 5
 set :rvm_type, :user
 # set :rvm_ruby_version, '2.3.1p112'
